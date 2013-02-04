9537efa305cf33a52e21adf4dab15f748ed98445
John Mark Diaz <excessivedemon@gmail.com>
2013-02-04 03:28:48 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friendfeed::Application.config.secret_token = '30174d8408fde7f3cb60b0ec329152dfca1cca465da6f313d77859c89cff06114634a5c3706d25af924f48ac815605e682cfbd3f2237ba1713e662825c12a473'
